c4e64cdceebd16ecfc7bf72d9ef5b2e45790905c
Allen Moore <allenwmoorejr@outlook.com>
2025-01-05 06:44:57 -0800

    changed the test

--- src/tests.py
+++ src/tests.py
@@ -6,7 +6,8 @@ class TestOperationsMethods(unittest.TestCase):
         self.operations = Operations()
 
     def test_operations(self):
-        self.assertEqual("Running app 'app1' in namespace 'dev' nice", self.operations.runningInfo("app1","dev"))
+        self.assertEqual("Running my app 'app1' in namespace 'dev' nice", self.operations.runningInfo("app1","dev"))
+
  
 if __name__ == '__main__':
     unittest.main()
